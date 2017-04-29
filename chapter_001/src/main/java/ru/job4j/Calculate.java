ad35a8e4f208a82ee0bfdd20b97b7d122594c985
MironovVadim <Mironov6292@gmail.com>
2017-04-29 18:35:33 +1000

    task 4086 with JavaDoc

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,3 +1,11 @@
+/**
+* Package for calculate task.
+*
+* @author Vadim Moronov (Mironov6292@gmail.ru/Multik6292@mail.ru)
+* @version $Id$
+* @since 0.1
+*/
+
 package ru.job4j;
 
 public class Calculate {
