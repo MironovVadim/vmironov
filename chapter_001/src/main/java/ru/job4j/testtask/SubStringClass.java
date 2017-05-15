6faf8177ddcf6bdd3004bba23d18c4bf7058811c
MironovVadim <Mironov6292@gmail.com>
2017-05-15 23:46:51 +1000

    Correction SubStringClass

--- chapter_001/src/main/java/ru/job4j/testtask/SubStringClass.java
+++ chapter_001/src/main/java/ru/job4j/testtask/SubStringClass.java
@@ -1,40 +1,39 @@
 /**
-* Searching substring in a string.
-*
-* @author Vadim Mironov (multik6292@mail.ru/mironov6292@gmail.ru)
-* @version $Id$
-* @since 0.1
-*/
+ * Searching substring in a string.
+ *
+ * @author Vadim Mironov (multik6292@mail.ru/mironov6292@gmail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
 
 package ru.job4j.testtask;
 
 /**
-* Class for searcheing substring in a string.
-*/
+ * Class for searcheing substring in a string.
+ */
 public class SubStringClass {
 	/**
-	* Method for searcheing substring in a string.
-	* @param origin - string for searcheing substring
-	* @param sub - substring
-	* @return boolean - is origin contains substring
-	*/
+	 * Method for searcheing substring in a string.
+	 * @param origin - string for searcheing substring
+	 * @param sub - substring
+	 * @return boolean - is origin contains substring
+	 */
 	public boolean contains(String origin, String sub) {
+		boolean isContains = false;
 		char[] originArray = origin.toCharArray();
 		char[] subArray = sub.toCharArray();
 		for (int i = 0; i < originArray.length - subArray.length; i++) {
-			if (originArray[i] == subArray[0]) {
-				boolean isContains = true;
-				for (int j = i + 1, t = 1; t < subArray.length; j++, t++) {
-					if (originArray[j] != subArray[t]) {
-						isContains = false;
-						break;
-					}
-				}
-				if (isContains) {
-					return true;
+			isContains = true;
+			for (int j = 0; j < subArray.length; j++) {
+				if (originArray[i + j] != subArray[j]) {
+					isContains = false;
+					break;
 				}
 			}
+			if (isContains) {
+				break;
+			}
 		}
-		return false;
+		return isContains;
 	}
 }
