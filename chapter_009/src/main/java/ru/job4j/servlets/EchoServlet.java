6850607729ed67b680f039871d2a121651af2f70
MironovVadim <Mironov6292@gmail.com>
2017-06-27 23:15:36 +1000

    Adding Servlet actions.

--- chapter_009/src/main/java/ru/job4j/servlets/EchoServlet.java
+++ chapter_009/src/main/java/ru/job4j/servlets/EchoServlet.java
@@ -1,5 +1,7 @@
 package ru.job4j.servlets;
 
+import java.util.Properties;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -7,16 +9,91 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
 /**
- * Created by ПК on 19.06.2017.
+ * Servlet.
  */
 public class EchoServlet extends HttpServlet {
+    /**
+     * Server address.
+     */
+    private static final String URL = "jdbc:postgresql:5432//localhost/UserDB";
+    /**
+     * Login and password.
+     */
+    private static final Properties PROPERTIES = new Properties();
+    static {
+        PROPERTIES.setProperty("login", "postgres");
+        PROPERTIES.setProperty("password", "user");
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
-        PrintWriter printWriter = new PrintWriter(resp.getOutputStream());
-        printWriter.append("hello world.");
-        printWriter.flush();
+        try (Connection connection = DriverManager.getConnection(URL, PROPERTIES)) {
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM user WHERE id = ?");
+            int id = Integer.parseInt(req.getParameter("id"));
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            PrintWriter pw = new PrintWriter(resp.getOutputStream());
+            while (resultSet.next()) {
+                pw.append("name:" + resultSet.getString("name")
+                        + " login:" + resultSet.getString("login")
+                        + " email: " + resultSet.getString("email"));
+                pw.flush();
+            }
+            resultSet.close();
+            preparedStatement.close();
+            pw.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        try (Connection connection = DriverManager.getConnection(URL, PROPERTIES)) {
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO user (name, login, email) VALUES (?, ?, ?)");
+            preparedStatement.setString(1, req.getParameter("name"));
+            preparedStatement.setString(2, req.getParameter("login"));
+            preparedStatement.setString(3, req.getParameter("email"));
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        try (Connection connection = DriverManager.getConnection(URL, PROPERTIES)) {
+            PreparedStatement preparedStatement = connection.prepareStatement("UPDATE user SET email = ? WHERE name = ?");
+            preparedStatement.setString(1, req.getParameter("email"));
+            preparedStatement.setString(2, req.getParameter("name"));
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        try (Connection connection = DriverManager.getConnection(URL, PROPERTIES)) {
+            PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM user WHERE name = ?");
+            preparedStatement.setString(1, req.getParameter("name"));
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ chapter_009/src/main/resources/populateUserTable.sql
@@ -0,0 +1,18 @@
+INSERT INTO public.User (NAME, LOGIN, EMAIL) VALUES (
+  'Vadim', 'VadimUser', 'Multik6292@mail.ru'
+);
+INSERT INTO public.User (NAME, LOGIN, EMAIL) VALUES (
+  'Dima', 'DimUser', 'Dima9922@mail.ru'
+);
+INSERT INTO public.User (NAME, LOGIN, EMAIL) VALUES (
+  'Yuri', 'YuriUser', 'Yuri@mail.ru'
+);
+INSERT INTO public.User (NAME, LOGIN, EMAIL) VALUES (
+  'Slava', 'SlavaUser', 'Slava@mail.ru'
+);
+INSERT INTO public.User (NAME, LOGIN, EMAIL) VALUES (
+  'Arkadii', 'ArkadiiUser', 'Arkadii@mail.ru'
+);
+INSERT INTO public.User (NAME, LOGIN, EMAIL) VALUES (
+  'Nikita', 'NikitaUser', 'Nikita0@mail.ru'
+);
