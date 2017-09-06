13cd7de9d3c8bea5f5637a3e71870ea82208a67b
MironovVadim <Mironov6292@gmail.com>
2017-09-07 01:56:46 +1000

    Создать TODO list[#23153] (FINAL) + repair

---
+++ chapter_010/src/test/java/ru/job4j/todolist/servlets/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package with servlets test classes.
+ *
+ * @author Vadim Moronov (Mironov6292@gmail.ru/Multik6292@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+package ru.job4j.todolist.servlets;
