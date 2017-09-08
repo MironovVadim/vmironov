149275b7cd599e5c88fbb8617943c6cc12fe72ec
Vadim Mironov <mironov6292@gmail.com>
2017-09-09 01:11:29 +1000

    Реализовать модели и связи для задание SQL хранилище машин. [#4744]

---
+++ chapter_010/src/main/java/ru/job4j/carstorage/Comment.java
@@ -0,0 +1,51 @@
+package ru.job4j.carstorage;
+
+import java.util.Date;
+
+public class Comment {
+    private int id;
+    private int userId;
+    private int carId;
+    private String description;
+    private Date date;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int user_id) {
+        this.userId = user_id;
+    }
+
+    public int getCarId() {
+        return carId;
+    }
+
+    public void setCarId(int carId) {
+        this.carId = carId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
---
+++ chapter_010/src/main/java/ru/job4j/carstorage/User.java
@@ -0,0 +1,60 @@
+package ru.job4j.carstorage;
+
+import java.util.Date;
+
+public class User {
+    private int id;
+    private String name;
+    private String secondName;
+    private String login;
+    private String password;
+    private Date registered;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getRegistered() {
+        return registered;
+    }
+
+    public void setRegistered(Date registered) {
+        this.registered = registered;
+    }
+}
