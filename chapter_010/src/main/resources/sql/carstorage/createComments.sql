d8e66fb4b866726e6410e426263c194a0e316610
Vadim Mironov <mironov6292@gmail.com>
2017-10-27 22:16:27 +0300

    annotation (2)

--- chapter_010/src/main/resources/sql/carstorage/createComments.sql
+++ chapter_010/src/main/resources/sql/carstorage/createComments.sql
@@ -1,8 +1,9 @@
-CREATE TABLE public.comments
+CREATE TABLE public.commentts
 (
-    id SERIAL NOT NULL PRIMARY KEY,
+    id SERIAL NOT NULL,
     user_id INT NOT NULL REFERENCES public.users(id),
     car_id INT NOT NULL REFERENCES public.cars(id),
     description VARCHAR(1000),
-    created_date TIMESTAMP DEFAULT now()
+    created_date TIMESTAMP DEFAULT now(),
+    PRIMARY KEY (id, car_id, user_id)
 );
